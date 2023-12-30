38ac45d1a4b692aac536bd9163a9ff678c6b06b1
SandyFit <sandy07r@gmail.com>
2023-12-30 11:51:55 -0500

    fetching data from tasty api

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
-const apiUrl = 'GET https://api.spoonacular.com/recipes/complexSearch';
 const themeBtn = document.querySelector('.nav-btn');
-const body = document.querySelector('body')
+const body = document.querySelector('body');
+
 
 const changeTheme = () => {
     body.classList.toggle('dark');
 }
-
 themeBtn.addEventListener('click', changeTheme);
+
+
+// ==================API CALL===================
+const options = {
+  method: 'GET',
+  url: 'https://tasty.p.rapidapi.com/recipes/list',
+  params: {
+    from: '0',
+    size: '20',
+    tags: 'under_30_minutes'
+  },
+  headers: {
+    'X-RapidAPI-Key': '2b647adabdmsh2da63f7dd8ace6dp10a31ejsn1fd6e9b10eb0',
+    'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+  }
+};
+
+const getAllRecipes = async() => {
+    try {
+        const response = await axios.request(options);
+        console.log(response.data);
+        const recipes = response.data.results;
+
+        const cardsBox = document.querySelector('#box');
+        recipes.forEach(recipe => {
+            cardsBox.innerHTML += `               
+                <div class="card">
+                    <p>Recipe: ${recipe.id}</p>
+                    <h3>${recipe.slug}</h3>
+                    <img class='card-img' src='${recipe.thumbnail_url}' alt="pic" />
+                    <p>${recipe.description}</p>
+                </div>                                  
+            `
+        });
+
+    }
+    
+    catch (error) {
+        console.error(error);
+    }
+
+
+}
+
+getAllRecipes();
+
